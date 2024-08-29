af4777d85f7a629fd28410c126780a51b32fc038
Axrich <116589250+Axrich@users.noreply.github.com>
2024-08-29 11:21:27 +0500

    .

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,25 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
-    }
-}
+        boolean result = true;
+        if (args == null || args.isEmpty()){
+            return false;
+        }
+        for (String elem : args) {
+            try {
+                double number = Double.parseDouble(elem);
+                if (Double.isNaN(number) || number <= 0) {
+                    result = false;
+                    break;
+                } }
+            catch (NumberFormatException | NullPointerException e){
+                result = false;
+            }
+        }
+        return result;
+    }}
+
+
+
+
+
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,16 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (str == null){
+            return false;
+        }
+        try {double number = Double.parseDouble(str);
+            if (number>0 && !Double.isNaN(number))
+            {return true;}
+            else {return false;}
+
+        }
+        catch (NumberFormatException | NullPointerException e)
+        {return false;}
     }
 }
